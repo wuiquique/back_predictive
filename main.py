9ae3a20c209c8936ef7242c6867f8be69d3f3d7e
wuiquique <luisenriquem15@gmail.com>
2024-08-15 00:14:35 -0600

    endpoint for predictive model

---
+++ main.py
@@ -0,0 +1,51 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+import shap
+
+model = joblib.load("../MSD_RFR_PredictiveModel.pkl")
+scaler = joblib.load("../MSD_RFR_Scaler.pkl")
+
+app = FastAPI()
+
+class SongModel(BaseModel):
+    year: int
+    duration: float
+    key: int
+    loudness: float
+    mode: int
+    tempo: float
+    signature: int
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/predict")
+def predict(data: SongModel):
+    df = pd.DataFrame([data.dict()])
+    df_scaled = scaler.transform(df)
+
+    prediction = model.predict(df_scaled)
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(df_scaled)
+
+    response = {
+        "prediction": prediction.tolist()[0],
+        "shap_values": {
+            "year": shap_values.tolist()[0][0],
+            "duration": shap_values.tolist()[0][1],
+            "key": shap_values.tolist()[0][2],
+            "loudness": shap_values.tolist()[0][3],
+            "mode": shap_values.tolist()[0][4],
+            "tempo": shap_values.tolist()[0][5],
+            "signature": shap_values.tolist()[0][6]
+        }
+    }
+
+    return response
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
