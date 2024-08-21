aaaf6faaef875174a2dc54532faba8f282be5ede
wuiquique <luisenriquem15@gmail.com>
2024-08-20 22:04:46 -0600

    cors update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pandas as pd
 import joblib
@@ -9,6 +10,18 @@ model = joblib.load("../MSD_RFR_PredictiveModel.pkl")
 scaler = joblib.load("../MSD_RFR_Scaler.pkl")
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class SongModel(BaseModel):
     year: int
